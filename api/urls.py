80954b0cb8687a673486b92eeec2512635188f0e
gabrielmoliva <moliva.gabriel@gmail.com>
2025-05-27 22:35:37 -0300

    created GET request for a movie given its id

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('movies', views.getMovies),
+    path('movies', views.getAllMovies),
     path('movies/add', views.addMovie),
-    path('movies/review', views.review)
+    path('movies/review', views.review),
+    path('movies/<int:movie_id>', views.getMovie)
 ]
--- api/views.py
+++ api/views.py
@@ -2,13 +2,24 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from base.models import Movie, Review
 from .serializers import MovieSerializer, MoviePostSerializer, ReviewSerializer
+from rest_framework import status
 
 
 @api_view(['GET'])
-def getMovies(request):
+def getAllMovies(request):
     movies = Movie.objects.all()
     serializer = MovieSerializer(movies, many=True)
 
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getMovie(request, movie_id):
+    try:
+        movie = Movie.objects.filter(pk=movie_id).get()
+    except Movie.DoesNotExist:
+        return Response({'detail': 'Movie not found.'}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = MovieSerializer(movie)
     return Response(serializer.data)
 
 @api_view(['POST'])
@@ -24,9 +35,9 @@ def addMovie(request):
 def review(request):
     serializer = ReviewSerializer(data=request.data)
     if serializer.is_valid():
-        movieId = serializer.validated_data['movie'].id
+        movie_id = serializer.validated_data['movie'].id
         newVoteScore = serializer.validated_data['score']
-        movie = Movie.objects.filter(pk=movieId).get()
+        movie = Movie.objects.filter(pk=movie_id).get()
         movie.updateScore(newVoteScore)
         serializer.save()
         return Response(serializer.data, status=201)
